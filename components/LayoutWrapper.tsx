542469f3d4652ae17d2a84632b5c24996b1cd41e
Praviandy Gustandika <praviandyg@gmail.com>
2022-12-23 23:25:46 +0700

    init

---
+++ components/LayoutWrapper.tsx
@@ -0,0 +1,14 @@
+﻿import {PageContainer} from "./PageContainer";
+import {Navbar} from "./Navbar";
+import React from "react";
+
+const LayoutWrapper = ({children}: {children: React.ReactNode}) => {
+  return (
+    <PageContainer>
+      <Navbar />
+      <main className="mb-auto">{children}</main>
+    </PageContainer>
+  )
+} 
+
+export default LayoutWrapper
---
+++ components/Navbar.tsx
@@ -0,0 +1,19 @@
+﻿import Link from "next/link";
+import Image from "next/image";
+import GeovLogo from "../public/cropped-vartha-research.png" 
+
+export const Navbar = () => {
+  return (
+    <div className="flex h-screen flex-col justify-between">
+      <header className="flex items-center justify-between py-10">
+        <Link href={"/"}>
+          <div className="flex items-center justify-between">
+            <div className="mr-3">
+              <Image src={GeovLogo} alt={"Geovartha Logo"} />
+            </div>
+          </div>
+        </Link>
+      </header>
+    </div>
+  )
+}
