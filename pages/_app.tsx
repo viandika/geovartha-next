88ba8bae07796f693bca8c756246bbebab8459c4
Praviandy Gustandika <praviandyg@gmail.com>
2023-01-12 20:02:14 +0700

    added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import LayoutWrapper from "../components/LayoutWrapper";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <LayoutWrapper>
       <Component {...pageProps} />
+      <Analytics />
     </LayoutWrapper>
   );
 }
