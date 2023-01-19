93445e752fc05917421aa07d969a21253ed710ba
Praviandy Gustandika <praviandyg@gmail.com>
2023-01-19 20:51:27 +0700

    added static 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,20 @@
+﻿import { ApiError404PageError404Page } from "./schemas";
+import { fetchStrapiAPI } from "../lib/strapiApi";
+
+export default function Custom404({notFound}: {notFound: ApiError404PageError404Page}) {
+  return <h1>{notFound.attributes.ErrorMessage}</h1>
+}
+
+export async function getStaticProps() {
+  // Run API calls in parallel
+  const [notFoundRes] = await Promise.all([
+    fetchStrapiAPI("/error-404-page", {
+      populate: "*",
+    }),
+  ]);
+  return {
+    props: {
+      notFound: notFoundRes.data,
+    },
+  };
+}
